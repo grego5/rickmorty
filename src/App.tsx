7f0b21c267fcc69b7d6a31fbfb7cb9134be3a724
Gregory <grgrsm@gmail.com>
2019-05-02 20:41:48 +0300

    delete fevorite button

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,102 @@
-import React from 'react';
-import {Store} from './store';
-import { IEpisode } from './interfaces';
-
+import React from "react";
+import { Store } from "./store";
+import { IEpisode } from "./interfaces";
 
 function App() {
-  const {state, dispatch} = React.useContext(Store);
-  
+  const { state, dispatch } = React.useContext(Store);
+
   React.useEffect(() => {
-    state.episodes.length === 0 && fetchDataAction()
-  })
+    state.episodes.length === 0 && fetchDataAction();
+  });
 
   const fetchDataAction = async () => {
     const URL = `https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes`;
     const res = await fetch(URL);
     const data = await res.json();
     return dispatch({
-      type: 'FETCH_DATA',
+      type: "FETCH_DATA",
       data: data._embedded.episodes
-    })
-  }
+    });
+  };
 
   const toggleAddFav = (episode: IEpisode) => {
     if (state.favorites.includes(episode)) {
-      return dispatch({
-        type: 'DEL_FAV',
-        data: episode
-      })
+      deleteFav(episode);
+    } else {
+      addFav(episode);
     }
+  };
+
+  const addFav = (episode: IEpisode) => {
     return dispatch({
-      type: 'ADD_FAV',
+      type: "ADD_FAV",
       data: episode
-    })
-  }
-  
-  const favList = state.favorites.map((episode: IEpisode, n: number) =>
-    <li key={episode.id}><a href={episode.url}>{n+1}. {episode.name}</a></li>
-  );
+    });
+  };
+
+  const deleteFav = (episode: IEpisode) => {
+    return dispatch({
+      type: "DEL_FAV",
+      data: episode
+    });
+  };
+
+  const favList = state.favorites.map((episode: IEpisode, n: number) => (
+    <li key={episode.id} className="favorites__item">
+      <a href={episode.url} className="favorites__link">
+        {n + 1}. {episode.name}
+      </a>
+      <div className="favorites__delete" onClick={() => deleteFav(episode)}>
+        &times;
+      </div>
+    </li>
+  ));
 
   return (
     <React.Fragment>
       <header className="header">
-        <div>
-          <h1>Rick and Morty</h1>
+        <h1>Rick and Morty</h1>
+        <div className="favorites">
           <p>Pick your favorite episode</p>
+          <button className="favorites__icon">
+            &hearts;
+            <span className="favorites__count">{state.favorites.length}</span>
+            <ul className="favorites__list">
+              {favList.length ? (
+                favList
+              ) : (
+                <li className="favorites__item">List is empty</li>
+              )}
+            </ul>
+          </button>
         </div>
-        <button className="like-icon">&hearts;
-          <span className="like-icon__count">{state.favorites.length}</span>
-          <ul className="like-icon__list">
-            {favList.length ? favList : "List is empty"}
-          </ul>
-        </button>
       </header>
       <section className="section-episodes">
         {state.episodes.map((episode: IEpisode) => {
           return (
             <div key={episode.id} className="episode">
-              <a href={episode.url} className="episode__link" title={episode.name}>
+              <a
+                href={episode.url}
+                className="episode__link"
+                title={episode.name}>
                 <h2 className="episode__title">{episode.name}</h2>
-                <img src={episode.image.medium} alt="episode thumbnail" className="episode__image"/>
+                <img
+                  src={episode.image.medium}
+                  alt="episode thumbnail"
+                  className="episode__image"
+                />
               </a>
-              <button className="episode__like-btn" onClick={ ()=>toggleAddFav(episode) }>&hearts;</button>
+              <button
+                className="episode__like-btn"
+                onClick={() => toggleAddFav(episode)}>
+                &hearts;
+              </button>
             </div>
-          )
+          );
         })}
       </section>
     </React.Fragment>
-  )
+  );
 }
 
 export default App;
